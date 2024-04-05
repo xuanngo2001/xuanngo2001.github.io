7a017b108d14e122148ebe8530d452922632943d
Xuan Ngo <xngo@genetec.com>
2024-04-05 09:34:58 -0400

    m

---
+++ _posts/2024-04-05-add-new-a-collection.md
@@ -0,0 +1,4 @@
+---
+layout: post
+title:  "Add new a collection"
+---
---
+++ _posts/post-create.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]     [string]    $Title
+)
+
+$today = Get-Date -Format "yyyy-MM-dd" # Output: 2020-05-17
+
+$title_formattted = $Title.ToLower() -replace " ","-"
+$front_matter = @"
+---
+layout: post
+title:  "${Title}"
+---
+"@
+
+$out_md_file = "${today}-${title_formattted}.md"
+
+$front_matter | Out-File $out_md_file
